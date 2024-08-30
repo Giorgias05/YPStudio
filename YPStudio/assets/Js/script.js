27c91e12737794743c2b4c67b244ccb9dfc6824b
Giorgia <121100010+Giorgias05@users.noreply.github.com>
2024-08-30 12:34:46 +0200

    Add files via upload

---
+++ YPStudio/assets/Js/script.js
@@ -0,0 +1,32 @@
+document.getElementById("hamburger").onclick = function () {
+    var navbar = document.getElementById("nav");
+    navbar.classList.toggle("show");
+};
+
+var navLinks = document.querySelectorAll("nav a");
+navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+        var navbar = document.getElementById("nav");
+        navbar.classList.toggle("show");
+    });
+});
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const offset = 152;
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - offset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
